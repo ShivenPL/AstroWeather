87871d612c05576b45819e5f9740b129ae0bb235
ShivenPL <krystian.kulesza96pl@gmail.com>
2019-06-05 16:22:13 +0200

    ...

--- app/src/main/java/com/example/astroweather/MoonFragment.java
+++ app/src/main/java/com/example/astroweather/MoonFragment.java
@@ -3,6 +3,7 @@ package com.example.astroweather;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
